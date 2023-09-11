1fc0d950708bbd535010a9f36ba200927b12a0e6
Alina <79420032+alina-multi@users.noreply.github.com>
2023-09-11 11:05:24 +0100

    update findMoviesInMovieList

--- src/js/library/findMoviesInMovieList.js
+++ src/js/library/findMoviesInMovieList.js
@@ -1,13 +1,21 @@
 export function findMoviesInMovieList(movieList, list) {
+  console.log(movieList);
   if (!movieList || !list || list.length === 0 ) return null;
 
   const listForRender = [];
-  for(i = 0; i < list.length; i += 1) {
-    const movie = movieList.find((movie) => movie.id === list[i].id);
+
+  list.forEach((item) => {
+    const movie = movieList.find((movie) => movie.id === item.id);
     if (movie) {
       listForRender.push(movie);
     }
-  }
+  });
+  // for(i = 0; i < list.length; i += 1) {
+  //   const movie = movieList.find((movie) => movie.id === list[i].id);
+  //   if (movie) {
+  //     listForRender.push(movie);
+  //   }
+  // }
 
   return listForRender;
 }
